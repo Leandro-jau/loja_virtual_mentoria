baa765f167d5165097580ffbb37ef166c5e687db
Leandro <Leandro@NTBIDATA-0001>
2025-02-27 21:51:52 -0300

    real 27/02/25 professor 13/09/22

--- src/main/java/jdev/mentoria/lojavirtual/service/NotaFiscalCompraService.java
+++ src/main/java/jdev/mentoria/lojavirtual/service/NotaFiscalCompraService.java
@@ -9,6 +9,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import jdev.mentoria.lojavirtual.model.dto.ObejtoRequisicaoRelatorioProdCompraNotaFiscalDTO;
+import jdev.mentoria.lojavirtual.model.dto.ObejtoRequisicaoRelatorioProdutoAlertaEstoque;
 
 
 @Service
@@ -51,6 +52,51 @@ public class NotaFiscalCompraService {
 		}
 		//BeanPropertyRowMapper = ele tenta fazer uma  conversão, ele verifica o resultado do nosso sql e transforma no objeto que nós precisamos vai colocar cada campo em um atributo e injeta os dados do sql no objeto para voltar a tela
 		retorno = jdbcTemplate.query(sql, new BeanPropertyRowMapper(ObejtoRequisicaoRelatorioProdCompraNotaFiscalDTO.class));
+		
+		return retorno;
+	}
+	
+	/**
+	 * Este relatório retorna os produtos que estão com estoque  menor ou igual a quantidade definida no campo de qtde_alerta_estoque.
+	 * @param alertaEstoque ObejtoRequisicaoRelatorioProdutoAlertaEstoque
+	 * @return  List<ObejtoRequisicaoRelatorioProdutoAlertaEstoque>  Lista de Objetos ObejtoRequisicaoRelatorioProdutoAlertaEstoque
+	 */
+	public List<ObejtoRequisicaoRelatorioProdutoAlertaEstoque> 
+					gerarRelatorioAlertaEstoque(ObejtoRequisicaoRelatorioProdutoAlertaEstoque alertaEstoque ){
+		
+		List<ObejtoRequisicaoRelatorioProdutoAlertaEstoque> retorno = new ArrayList<ObejtoRequisicaoRelatorioProdutoAlertaEstoque>();
+		
+		String sql = "select p.id as codigoProduto, p.nome as nomeProduto, "
+				+ " p.valor_venda as valorVendaProduto, ntp.quantidade as quantidadeComprada, "
+				+ " pj.id as codigoFornecedor, pj.nome as nomeFornecedor,cfc.data_compra as dataCompra, "
+				+ " p.qtd_estoque as qtdEstoque, p.qtde_alerta_estoque as qtdAlertaEstoque "
+				+ " from nota_fiscal_compra as cfc "
+				+ " inner join nota_item_produto as ntp on  cfc.id = nota_fiscal_compra_id "
+				+ " inner join produto as p on p.id = ntp.produto_id "
+				+ " inner join pessoa_juridica as pj on pj.id = cfc.pessoa_id where ";
+		
+		sql += " cfc.data_compra >='"+alertaEstoque.getDataInicial()+"' and ";
+		sql += " cfc.data_compra <= '" + alertaEstoque.getDataFinal() +"' ";
+		sql += " and p.alerta_qtde_estoque = true and p.qtd_estoque <= p.qtde_alerta_estoque "; 
+		
+		if (!alertaEstoque.getCodigoNota().isEmpty()) {
+		 sql += " and cfc.id = " + alertaEstoque.getCodigoNota() + " ";
+		}
+		
+		
+		if (!alertaEstoque.getCodigoProduto().isEmpty()) {
+			sql += " and p.id = " + alertaEstoque.getCodigoProduto() + " ";
+		}
+		
+		if (!alertaEstoque.getNomeProduto().isEmpty()) {
+			sql += " upper(p.nome) like upper('%"+alertaEstoque.getNomeProduto()+"')";
+		}
+		
+		if (!alertaEstoque.getNomeFornecedor().isEmpty()) {
+			sql += " upper(pj.nome) like upper('%"+alertaEstoque.getNomeFornecedor()+"')";
+		}
+		
+		retorno = jdbcTemplate.query(sql, new BeanPropertyRowMapper(ObejtoRequisicaoRelatorioProdutoAlertaEstoque.class));
 		
 		return retorno;
 	}
